d9ad3ec71bda5c516db5b66dd3ed33658e4d0862
Itm0z 🍪 <doudou.mroczekle2@gmail.com>
2025-06-26 21:43:22 +0200

    Ajouts d'ISO (Linux)
    Ajouts d'ISO !
    - Ubuntu 24.04 LTS + 25.04 x64 Multilangue
    - Debian 12.11 Bookworm x64 Multilangue

--- script.js
+++ script.js
@@ -18,29 +18,37 @@ const data = {
   },
   linux: {
     "Ubuntu": {
-      editions: ["22.04 LTS", "20.04 LTS"],
-      languages: ["Français", "Anglais"]
+      editions: ["24.04 LTS", "25.04"],
+      languages: ["Multilangue"]
     },
     "Debian": {
-      editions: ["12 Bookworm", "11 Bullseye"],
-      languages: ["Français", "Anglais"]
+      editions: ["12.11 Bookworm"],
+      languages: ["Multilangue"]
     }
   }
 };
 
 const downloadLinks = {
+    // Français :
+    // Windows 7
     "windows_Windows 7_Professional_x86_Français": "https://archive.org/download/win-7-pro-french-COEM/Win7_Pro_SP1_French_COEM_x86.iso",
     "windows_Windows 7_Professional_x64_Français": "https://archive.org/download/win-7-pro-french-COEM/Win7_Pro_SP1_French_COEM_x64.iso",
     "windows_Windows 7_Ultimate_x64_Français": "https://archive.org/download/Windows7UltimateSP1x64French/fr_windows_7_ultimate_with_sp1_x64_dvd_u_677299.iso",
     "windows_Windows 7_Ultimate_x86_Français": "https://archive.org/download/Windows7UltimateSP1x86French/fr_windows_7_ultimate_with_sp1_x86_dvd_u_677434.iso",
     "windows_Windows 7_Home Premium_x64_Français": "https://archive.org/download/windows-7-home-premium-french/Win7_HomePrem_SP1_French_x64.iso",
     "windows_Windows 7_Home Premium_x86_Français": "https://archive.org/download/windows-7-home-premium-french/Win7_HomePrem_SP1_French_x32.iso",
+    // Windows 10
     "windows_Windows 10_Home_x64_Français": "https://archive.org/download/win-10-22-h-2-french-x-64/Win10_22H2_French_x64.iso",
     "windows_Windows 10_Pro_x64_Français": "https://archive.org/download/win-10-22-h-2-french-x-64/Win10_22H2_French_x64.iso",
     "windows_Windows 10_Enterprise_x64_Français": "https://archive.org/download/win-10-22-h-2-french-x-64/Win10_22H2_French_x64.iso",
     "windows_Windows 10_Home_x86_Français": "https://archive.org/download/win-10-22-h-2-french-x-32/Win10_22H2_French_x32.iso",
     "windows_Windows 10_Pro_x86_Français": "https://archive.org/download/win-10-22-h-2-french-x-32/Win10_22H2_French_x32.iso",
-    "windows_Windows 10_Enterprise_x86_Français": "https://archive.org/download/win-10-22-h-2-french-x-32/Win10_22H2_French_x32.iso"
+    "windows_Windows 10_Enterprise_x86_Français": "https://archive.org/download/win-10-22-h-2-french-x-32/Win10_22H2_French_x32.iso",
+    // Ubuntu 
+    "linux_Ubuntu_24.04 LTS_x64_Multilangue": "https://releases.ubuntu.com/24.04.2/ubuntu-24.04.2-desktop-amd64.iso",
+    "linux_Ubuntu_25.04_x64_Multilangue": "https://releases.ubuntu.com/25.04/ubuntu-25.04-desktop-amd64.iso",
+    // Debian
+    "linux_Debian_12.11 Bookworm_x64_Multilangue": "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.11.0-amd64-netinst.iso"
   };  
 
 function resetSelect(selectElement) {
